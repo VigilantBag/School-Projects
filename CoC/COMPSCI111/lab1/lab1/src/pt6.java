f646860117c0c1581c1c37480d110c7ebf891e27
VigilantBag <vigilantbag2018@gmail.com>
2021-06-21 11:49:04 -0700

    pt6 fix

--- CoC/COMPSCI111/lab1/lab1/src/pt6.java
+++ CoC/COMPSCI111/lab1/lab1/src/pt6.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class pt6 {
     public static void main(String[] args) {
+        //Have user enter a number, then double it
         System.out.println("Enter a number");
         Scanner input = new Scanner (System.in);
         int number = input.nextInt();
