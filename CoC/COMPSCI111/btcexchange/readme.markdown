c6c83c792016364e3ec57ade20a10a8d1717933d
VigilantBag <vigilantbag2018@gmail.com>
2021-07-08 19:59:10 -0700

    lab 3 start

---
+++ CoC/COMPSCI111/btcexchange/readme.markdown
@@ -0,0 +1,5 @@
+# Bitcoin Exchange
+
+## A barebones simple console based exchange simulator using live prices.
+
+Coin Gecko Java API provided by Philipinho <https://github.com/Philipinho/CoinGecko-Java>
---
+++ CoC/COMPSCI111/btcexchange/src/main/java/com/vigilantbag/App.java
@@ -0,0 +1,27 @@
+package com.vigilantbag;
+
+import java.util.Map;
+
+import com.litesoftwares.coingecko.CoinGeckoApiClient;
+import com.litesoftwares.coingecko.constant.Currency;
+import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl;
+
+public class App {
+    private static class getLive {
+
+       private String price () {
+            CoinGeckoApiClient client = new CoinGeckoApiClientImpl();
+            Map p = client.getPrice("bitcoin",Currency.USD);
+            String pString = p.toString(); 
+            String psClean1 = pString.replace("{bitcoin={usd=", "");
+            return psClean1.replace("}}", "");
+        }
+
+    }
+    public static void main(String[] args) {
+        getLive gL = new getLive();
+
+        System.out.println(gL.price());
+        
+    }
+}
